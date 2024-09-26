c32b1de30895318df7bf31843f7817942015959a
Shivam <schaurasia@infoane.com>
2024-09-26 16:17:30 +0530

    update

--- hero5/marvel/views.py
+++ hero5/marvel/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def spiderman(request):
-    return render(request,'marvel/spiderman.html')
+    context={'superhero':'spiderman'}
+    return render(request,'marvel/spiderman.html',context)
