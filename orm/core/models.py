54a9bf87bcbcc826d31c4b840d9e89959b04ce7e
Shivam <schaurasia@infoane.com>
2024-09-27 18:26:20 +0530

    update

--- orm/core/models.py
+++ orm/core/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class Marvel(models.Model):
     name = models.CharField(max_length=50),
-    city = models.CharField(max_length=50),
+    city = models.CharField(max_length=50,default='null')
 
-    def __str__(self):
-        return self.name
+   
+class M(models.Model):
+    x= models.IntegerField()
