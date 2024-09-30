5880a72e7f4079e047f7757fb912b730436459f7
Shivam <schaurasia@infoane.com>
2024-09-30 17:29:05 +0530

    github

--- orm/core/views.py
+++ orm/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from . models import Marvel
 # Create your views here.
 
 def index(request):
-    return render(request,'core/index.html')
+    m = Marvel.objects.all()
+    return render(request,'core/index.html',{'marvel':m})
