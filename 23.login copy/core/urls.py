3e3cc3e1eee50ba7f2e9665fe42a11c650e2681d
Sandip <jaiswarsandeep00@gmail.com>
2024-10-22 12:37:07 +0530

    update

--- 23.login copy/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.signup,name='signup'),
-    path('login/',views.user_login,name='login'),
-    path('profile/',views.user_profile,name='profile'),
-    path('logout/',views.user_logout,name='logout'),
-]
