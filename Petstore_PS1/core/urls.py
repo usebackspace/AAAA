90dda2cec29fedf338458eeee03470bafda56042
Sandip <jaiswarsandeep00@gmail.com>
2024-10-24 21:03:24 +0530

    update

--- Petstore_PS1/core/urls.py
+++ Petstore_PS1/core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('dogcategories',views.dog_categories,name='dogcategories'),
 ]
--- Petstore_PS1/core/views.py
+++ Petstore_PS1/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'core/index.html')
+
+def dog_categories(request):
+    return render(request,'core/dogcategories.html')
